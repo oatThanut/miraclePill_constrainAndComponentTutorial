b4e02a7de7ffb9af08d1869394e420d26fc6e284
oatThanut <oatThanut@Thanuts-MacBook-Pro.local>
2017-03-04 23:26:09 +0700

    add constrain and set UIPickerView

--- MiraclePill/ViewController.swift
+++ MiraclePill/ViewController.swift
@@ -8,18 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var statePicker: UIPickerView!
+    
+    @IBOutlet weak var statePickerBtn: UIButton!
+    
+    let state = ["Alaska", "Arkansas", "Alabama", "California"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        statePicker.dataSource = self
+        statePicker.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func stateButtonPressed(_ sender: AnyObject) {
+        
+        statePicker.isHidden = false
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+        return state.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        return state[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        statePickerBtn.setTitle(state[row], for: UIControlState())
+        statePicker.isHidden = true
+    }
 
 }
 
